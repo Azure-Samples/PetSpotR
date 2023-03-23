adb5431e9cc6c6258a3f30475a21514dbee80ba4
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-03-23 06:01:19 +0000

    Update create commands

--- .devcontainer/post-setup.sh
+++ .devcontainer/post-setup.sh
@@ -1,5 +1,1 @@
-# Install python dependencies
-pip install -r /workspace/src/backend/requirements.txt
-
-# dotnet restore
-dotnet restore /workspace/src/frontend/PetSpotR/PetSpotR.csproj
+echo "Done!"
